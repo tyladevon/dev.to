126ab8018f3e6aa6d66517e6f236667ed451c680
Zachary Nemeroff <zanemeroff@gmail.com>
2020-03-23 22:13:00 -0600

    Add create method to controller
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>
    Co-Authored-By: Harrison <harrison483@gmail.com>

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,17 @@
 class CollectionsController < ApplicationController
+
   def index
     @collections = UserCollection.where(user_id: session_current_user_id)
   end
+
+  def create
+    collection = UserCollection.create(title: params[:title], user_id: session_current_user_id)
+    if collection.save
+      redirect_to "/collections"
+    else
+      flash[:error] = collection.errors.full_messages.to_sentence
+      redirect_to "/collections"
+    end
+  end
+
 end
