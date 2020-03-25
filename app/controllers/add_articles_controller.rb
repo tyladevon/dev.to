273ba2749ccc2acdb762dc7ecdbc7f5c692d082c
Harrison Levin <harrison483@gmail.com>
2020-03-24 21:24:09 -0600

    change default name

--- app/controllers/add_articles_controller.rb
+++ app/controllers/add_articles_controller.rb
@@ -1,8 +1,8 @@
 class AddArticlesController < ApplicationController
     def create
-      collection = UserCollection.where(title: "New Default Collection").where(user_id: session_current_user_id).first
+      collection = UserCollection.where(title: "Default Collection").where(user_id: session_current_user_id).first
       if !collection
-        collection = UserCollection.create(title: "New Default Collection", user_id: session_current_user_id)
+        collection = UserCollection.create(title: "Default Collection", user_id: session_current_user_id)
       end
       article = Article.find(params[:reactable_id])
       collection.articles << article
