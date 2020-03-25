ed617a9325f3a1d1a5aaac2136cb249f09326016
Harrison Levin <harrison483@gmail.com>
2020-03-24 21:13:21 -0600

    spec for api/user_collections

---
+++ spec/requests/api/v0/user_collections_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "Api::V0::UserCollectionsController", type: :request do
+  before do
+    @user = create(:user)
+    create(:user_collection, user_id: @user.id)
+    create(:user_collection, user_id: @user.id)
+    create(:user_collection, user_id: @user.id)
+  end
+
+  describe "GET /api/user_collections" do
+    it "returns collections for the user" do
+      get '/api/user_collections', params: { user_id: @user.id }
+
+      expect(response).to be_successful
+
+      collections = JSON.parse(response.body)['collections']
+      expect(collections.count).to eq(3)
+    end
+  end
+end
