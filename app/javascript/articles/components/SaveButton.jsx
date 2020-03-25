7d5d9d0db0267c0ab805cc0ba03aa7f6b5b4b3ce
Harrison Levin <harrison483@gmail.com>
2020-03-24 21:12:09 -0600

    save button with fetch code - not working

--- app/javascript/articles/components/SaveButton.jsx
+++ app/javascript/articles/components/SaveButton.jsx
@@ -1,13 +1,32 @@
 import { h, Component } from 'preact';
 import PropTypes from 'prop-types';
 import { articlePropTypes } from '../../src/components/common-prop-types';
-
 export class SaveButton extends Component {
-  componentDidMount() {
-    const { isBookmarked } = this.props;
-    this.setState({ buttonText: isBookmarked ? 'SAVED' : 'SAVE' });
+  constructor() {
+    super();
+    this.state =
+      { collectionDropbox: 'all articles' }
   }
 
+  componentDidMount() {
+      fetch('http://localhost:3000/api/user_collections')
+        .then(response => response.json())
+        .then(data => {
+          this.setState({creatures: data})
+        })
+        .catch(error => window.alert(`Opps! Something went wrong! ${error}`))
+
+
+    const { isBookmarked } = this.props;
+    this.setState({ buttonText: isBookmarked ? 'SAVED' : 'SAVE!' });
+  }
+
+  updateDropboxState = collectionName => {
+    this.setState({collectionDropbox: collectionName});
+  }
+
+  // collectionTitles variable will represent an array of all collection titles
+  // this.state.collectionDropbox references current selected collection title
   render() {
     const { buttonText } = this.state;
     const { article, isBookmarked, onClick } = this.props;
@@ -19,26 +38,38 @@ export class SaveButton extends Component {
         this.setState({ buttonText: 'UNSAVE' });
       }
     };
-
+    let collectionTitles = ['for fun', 'weekend reads', 'save for mom']
+    let printCollectionTitles = collectionTitles.map(title => {
+      return <option value={title}>{title}</option>
+    })
     if (article.class_name === 'Article') {
       return (
-        <button
-          type="button"
-          className={`article-engagement-count engage-button bookmark-button ${
-            isBookmarked ? 'selected' : ''
-          }`}
-          data-initial-feed
-          data-reactable-id={article.id}
-          onClick={onClick}
-          onMouseOver={mouseOver}
-          onFocus={mouseOver}
-          onMouseout={mouseOut}
-          onBlur={mouseOut}
-        >
-          <span className={isBookmarked ? 'bm-success' : 'bm-initial'}>
-            {buttonText}
-          </span>
-        </button>
+        <div className='save-to-collection-button-container'>
+            <div className='collection-dropdown-container'>
+              <label className='collection-dropdown-heading'>collection:</label>
+              <select className='collection-dropbox' onChange={e => this.updateDropboxState(e.target.value)}>
+                <option value='all articles'>all articles</option>
+                {printCollectionTitles}
+              </select>
+            </div>
+          <button
+            type="button"
+            className={`article-engagement-count engage-button bookmark-button ${
+              isBookmarked ? 'selected' : ''
+            }`}
+            data-initial-feed
+            data-reactable-id={article.id}
+            onClick={onClick}
+            onMouseOver={mouseOver}
+            onFocus={mouseOver}
+            onMouseout={mouseOut}
+            onBlur={mouseOut}
+          >
+            <span className={isBookmarked ? 'bm-success' : 'bm-initial'}>
+              {buttonText}
+            </span>
+          </button>
+        </div>
       );
     }
     if (article.class_name === 'User') {
@@ -54,15 +85,12 @@ export class SaveButton extends Component {
         </button>
       );
     }
-
     return null;
   }
 }
-
 SaveButton.propTypes = {
   article: articlePropTypes.isRequired,
   isBookmarked: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
-
 SaveButton.displayName = 'SaveButton';
