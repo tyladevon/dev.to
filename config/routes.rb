6bebf82240129387794ca09da874fa6b06876a87
Harrison Levin <harrison483@gmail.com>
2020-03-24 21:12:49 -0600

    route for api endpoint - user_controller

--- config/routes.rb
+++ config/routes.rb
@@ -103,6 +103,7 @@ Rails.application.routes.draw do
           get "me(/:status)", to: "articles#me", as: :me, constraints: { status: /published|unpublished|all/ }
         end
       end
+      resources :user_collections, only: [:index]
       resources :comments, only: %i[index show]
       resources :chat_channels, only: [:show]
       resources :videos, only: [:index]
