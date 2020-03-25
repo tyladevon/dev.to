cbf52f3fd4b3e45f84d0181c78d037f047e2a839
Harrison Levin <harrison483@gmail.com>
2020-03-24 21:23:44 -0600

    prepend default collection

--- app/controllers/api/v0/user_collections_controller.rb
+++ app/controllers/api/v0/user_collections_controller.rb
@@ -3,6 +3,7 @@ module Api
     class UserCollectionsController < ApiController
       def index
         collections = UserCollection.where(user_id: params[:user_id]).pluck(:title)
+        collections.prepend("Default Collection")
         render json: { collections: collections }
       end
     end
