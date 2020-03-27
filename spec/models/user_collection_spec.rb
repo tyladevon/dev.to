28187474d5ca165fcafed40080ebf92f53aaceb8
Tyla Devon <tyladevon@gmail.com>
2020-03-27 15:13:19 -0600

    Add title validation test for User Collection model

--- spec/models/user_collection_spec.rb
+++ spec/models/user_collection_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe UserCollection, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_uniqueness_of :title }
   end
 
   describe "relationships" do
