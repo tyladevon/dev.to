9cf9fb4cfca58c17253c90b1968bb94eaec55c51
Harrison Levin <harrison483@gmail.com>
2020-03-27 13:18:17 -0600

    Add validates uniqueness of title per (scope) user

--- app/models/user_collection.rb
+++ app/models/user_collection.rb
@@ -1,6 +1,8 @@
 class UserCollection < ApplicationRecord
-  validates :title, presence: true
+  #validates :title, presence: true
   belongs_to :user
   has_many :article_collections
   has_many :articles, through: :article_collections
+  validates :title, presence: true
+  validates_uniqueness_of :title, scope: :user_id, case_sensitive: false
 end
