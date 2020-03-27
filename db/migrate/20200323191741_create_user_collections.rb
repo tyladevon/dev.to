3e55a5ffb354e0eb60fe839d8d6de600118dd1fd
Harrison Levin <harrison483@gmail.com>
2020-03-27 13:18:36 -0600

    Create additional index on user_id

--- db/migrate/20200323191741_create_user_collections.rb
+++ db/migrate/20200323191741_create_user_collections.rb
@@ -3,7 +3,9 @@ class CreateUserCollections < ActiveRecord::Migration[5.2]
     create_table :user_collections do |t|
       t.string :title
       t.references :user, foreign_key: true
-      t.timestamps 
+      t.timestamps
+
+      add_index(:user_collections, :user)
     end
   end
 end
