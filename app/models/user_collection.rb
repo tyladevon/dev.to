ab4e75183d19ecc4ac3632d732dc7c5f4a68acbe
Tyla Devon <tyladevon@gmail.com>
2020-03-27 18:35:58 -0600

    Refactor UserCollection model  to remove duplicated title validation.

--- app/models/user_collection.rb
+++ app/models/user_collection.rb
@@ -1,8 +1,7 @@
 class UserCollection < ApplicationRecord
-  validates :title, presence: true, uniquness: { scope: :user_id }
+  validates :title, presence: true
+  validates_uniqueness_of :title, scope: :user_id, case_sensitive: false
   belongs_to :user
   has_many :article_collections
   has_many :articles, through: :article_collections
-  validates :title, presence: true
-  validates_uniqueness_of :title, scope: :user_id, case_sensitive: false
 end
